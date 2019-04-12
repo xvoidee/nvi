ebe18e6657ea5aa2fcae1a9fbc0bd629ac1dbe6e
xvoidee <alexander.chaika@gmail.com>
2019-04-12 15:57:46 +0200

    Added clang,nodejs,neovim

--- install.sh
+++ install.sh
@@ -1,115 +1,59 @@
-#!/bin/sh
+#!/bin/bash
 
 print_help() {
 	cat << EOF
 Options:
  --help                     display help
- --no-alias                 do not create alias in ~/.bashrc
- --no-ccls                  do not install ccls
- --ccls-path=<path>         target directory where ccls will be installed
-                            or directory with pre-installed ccls
-                            default /opt/nvimclipse_3rdparty/
- --no-clang                 do not install clang
- --clang-path=<path>        target directory where clang will be installed
-                            or directory with pre-installed clang
-                            default /opt/nvimclipse_3rdparty/
- --no-neovim                do not install neovim
- --neovim-path=<path>       target directory where neovim will be installed
-                            or directory with pre-installed neovim
-                            default /opt/nvimclipse_3rdparty/
- --no-nodejs                do not install nodejs
- --nodejs-path=<path>       target directory where nodejs will be installed
-                            default /opt/nvimclipse_3rdparty/
- --keep-cache               do not remove downloaded binaries
  --nvimclipse-path=<path>   target directory for nvimclipse home (configs, plugins)
                             default /opt/nvimclipse
- --3rdparty-path=<path>     target directory where 3rdparties are installed
+ --3rdparty-path=<path>     target directory for 3rdparties
                             default /opt/nvimclipse_3rdparty
+ --skip-nodejs-install      do not download nodejs, use system-provided instead
+ --nodejs-path=<path>       path to system-provided nodejs
+                            leave unset to autodetect from \$PATH
+ --skip-clang-install       do not download clang, use system-provided instead
+ --clang-path=<path>        path to system-provided clang
+                            leave unset to autodetect from \$PATH
+ --skip-neovim-install      do not download neovim, use system-provided instead
+ --neovim-path=<path>       path to system-provided neovim
+                            leave unset to autodetect from \$PATH
+ --skip-ccls-install        do not download and build ccls, use system-provided instead
+ --ccls-path=<path>         path to system-provided ccls
+                            leave unset to autodetect from \$PATH
 EOF
 }
 
-opts=`/usr/bin/getopt \
-	-o a,b,c,d:,e,f:,g,h:,j,k:,l:,m: \
-	-l help,no-alias,no-ccls,ccls-path:,no-clang,clang-path:,no-neovim,neovim-path:,no-nodejs,nodejs-path:,keep-cache,nvimclipse-home:,3rdparty-home: \
-	-- "$@" \
-`
+opts=`/usr/bin/getopt -o '' --long help,nvimclipse-path:,3rdparty-path:,skip-nodejs-install,nodejs-path:,skip-clang-install,clang-path:,skip-neovim-install,neovim-path:,skip-ccls-install,-ccls-path: -- "$@"`
 
-if [ $? != 0 ]; then
+if [ $? != 0 ] ; then
 	echo "getopt failed"
 	print_help
 	exit 1
 fi
 
-no_ccls=false
-ccls_path=$thirdparty_path
-
-no_clang=false
-clang_path=$thirdparty_path
-
-no_neovim=false
-neovim_path=$thirdparty_path
-
-no_nodejs=false
-nodejs_path=$thirdparty_path
-
-no_alias=false
-keep_cache=false
-
 nvimclipse_path=/opt/nvimclipse
 thirdparty_path=/opt/nvimclipse_3rdparty
 
+skip_nodejs_install=false
+nodejs_path=""
+
+skip_clang_install=false
+clang_path=""
+
+skip_neovim_install=false
+neovim_path=""
+
+skip_ccls_install=false
+ccls_path=""
+
 eval set -- "$opts"
-while true; do
-  case "$1" in
-		--help )
+while true ; do
+	case "$1" in
+		--help)
 			print_help
-			shift
-      ;;
-		--no-alias )
-			no_alias=true
-			shift
+			exit 0
 			;;
-		--no-ccls )
-			no_ccls=true
-			shift
-			;;
-		--ccls-path )
-			ccls_path=$2
-			shift
-			shift
-			;;
-		--no-clang )
-			no_clang=true
-			shift
-			;;
-		--clang-path )
-			clang_path=$2
-			shift
-			shift
-			;;
-		--no-neovim )
-			no_neovim=true
-			shift
-			;;
-		--neovim-path )
-			neovim_path=$2
-			shift
-			shift
-			;;
-		--no-nodejs )
-			no_nodejs=true
-			shift
-			;;
-		--nodejs-path )
-			nodejs_path=$2
-			shift
-			shift
-			;;
-		--keep-cache )
-			keep_cache=true
-			shift
-			;;
-		--nvimclipse-path )
+		--nvimclipse-path)
 			nvimclipse_path=$2
 			shift
 			shift
@@ -119,17 +63,86 @@ while true; do
 			shift
 			shift
 			;;
-		-- )
+		--skip-nodejs-install)
+			skip_nodejs_install=true
+			shift
+			;;
+		--nodejs-path)
+			nodejs_path=$2
+			shift
+			shift
+			;;
+		--skip-clang-install)
+			skip_clang_install=true
+			shift
+			;;
+		--clang-path)
+			clang_path=$2
+			shift
+			shift
+			;;
+		--skip-neovim-install)
+			skip_neovim_install=true
+			shift
+			;;
+		--neovim-path)
+			neovim_path=$2
+			shift
+			shift
+			;;
+		--skip-ccls-install)
+			skip_ccls_install=true
+			shift
+			;;
+		--ccls-path)
+			ccls_path=$2
+			shift
+			shift
+			;;
+		--)
 			shift
 			break
 			;;
-		* )
-			echo "Unknown option $1"
-			print_help
+		*)
+			echo "unknown error"
 			exit 1
 			;;
-  esac
+	esac
 done
+
+set_text_color() {
+	tput setaf $1
+}
+
+print_info() {
+	set_text_color 3
+	echo $1
+	set_text_color 0
+}
+
+print_success() {
+	set_text_color 2
+	echo $1
+	set_text_color 0
+}
+
+print_fail() {
+	set_text_color 1
+	echo $1
+	set_text_color 0
+}
+
+probe_command() {
+	command -v $1 > /dev/null
+}
+
+probe_mkdir() {
+	mkdir -p $1 > /dev/null
+	if [[ $? -eq 1 ]] ; then
+		print_fail "Unable to create folder $1"
+		prereqs_met=false
+	fi
+}
 
 install() {
 	if [ ! -f temp/$2 ]; then
@@ -141,112 +154,89 @@ install() {
 	mv temp/$folder $thirdparty_path/$3
 }
 
-install_clang() {
-	if ( $no_clang ); then
-		return
+prereqs_met=true
+
+probe_mkdir $thirdparty_path
+probe_mkdir $nvimclipse_path
+
+if ( ! $prereqs_met ) ; then
+	print_fail "Unable to create mandatory install directories, see above"
+	exit 1
+fi
+
+probe_binary() {
+	preinstalled_path=$1
+	binary_name=$2
+	relative_path=$3
+
+	p="\$PATH"
+	c=$binary_name
+
+	if [ ! -z $preinstalled_path ] ; then
+		p=$preinstalled_path/$relative_path
+		c=$preinstalled_path/$relative_path/$binary_name
 	fi
 
+	probe_command $c
+	if [[ $? = "1" ]] ; then
+		print_fail "$binary_name not found in $p"
+		prereqs_met=false
+	fi
+}
+
+if ( $skip_nodejs_install ) ; then
+	probe_binary \
+		"$nodejs_path" \
+		"node" \
+		"bin"
+	nodejs_path=""
+else
+	install \
+		"https://nodejs.org/dist/v10.15.3" \
+		"node-v10.15.3-linux-x64.tar.xz"
+	nodejs_path="$thirdparty_path/node-v10.15.3-linux-x64"
+fi
+
+if ( $skip_clang_install ) ; then
+	probe_binary \
+		"$clang_path" \
+		"clang" \
+		"bin"
+	clang_path=""
+else
 	install \
 		"http://releases.llvm.org/7.0.1" \
 		"clang+llvm-7.0.1-x86_64-linux-gnu-ubuntu-16.04.tar.xz"
-
 	clang_path="$thirdparty_path/clang+llvm-7.0.1-x86_64-linux-gnu-ubuntu-16.04"
-}
+fi
 
-install_neovim() {
-	if ( $no_neovim ); then
-		return
-	fi
-
+if ( $skip_neovim_install ) ; then
+	probe_binary \
+		"$neovim_path" \
+		"nvim" \
+		"bin"
+	neovim_path=""
+else
 	install \
 		"https://github.com/neovim/neovim/releases/download/v0.3.4" \
 		"nvim-linux64.tar.gz" \
 		"nvim-0.3.4"
-
 	neovim_path="$thirdparty_path/nvim-0.3.4"
-}
+fi
 
-install_ccls() {
-	if ( $no_ccls ); then
-		return
-	fi
+if ( $skip_ccls_install ) ; then
+	probe_binary \
+		"$ccls_path" \
+		"ccls" \
+		"bin"
+	ccls_path=""
+else
 
-	cmake_version="cmake-3.14.0-Linux-x86_64"
-	cmake_command="`pwd`/temp/$cmake_version/bin/cmake"
-	cmake_archive="$cmake_version.tar.gz"
-	if [ ! -f temp/$cmake_archive ]; then
-		wget -c "https://github.com/Kitware/CMake/releases/download/v3.14.0/$cmake_archive" -P temp
-	fi
-	tar -xf temp/$cmake_archive -C temp/
+fi
 
-	cd temp
-	git clone --depth=1 --recursive https://github.com/MaskRay/ccls
-	cd ccls
+if ( ! $prereqs_met ) ; then
+	print_fail "Unmet prerequisites, check messages above"
+fi
 
-	eval $cmake_command -H. -BRelease \
-		-DCMAKE_BUILD_TYPE=Release \
-		-DCMAKE_PREFIX_PATH=$clang_path \
-		-DCMAKE_CXX_COMPILER=$clang_path/bin/clang++
-	eval $cmake_command --build Release
-	mkdir $thirdparty_path/ccls
-	mkdir $thirdparty_path/ccls/bin
-	cp Release/ccls $thirdparty_path/ccls/bin
-	cd ../..
-	rm -rf temp/ccls
-	rm -rf temp/$cmake_version
+exit 0
 
-	ccls_path="$thirdparty_path/ccls"
-}
-
-install_nodejs() {
-	if ( $no_nodejs ); then
-		return
-	fi
-
-	install \
-		"https://nodejs.org/dist/v10.15.3" \
-		"node-v10.15.3-linux-x64.tar.xz"
-
-	nodejs_path="$thirdparty_path/node-v10.15.3-linux-x64"
-}
-
-install_nvimclipse() {
-	mkdir $nvimclipse_path/autoload
-	cp vim-plug/plug.vim $nvimclipse_path/autoload/
-
-	cp config/.cfg.*            $nvimclipse_path
-	cp config/.vimrc*           $nvimclipse_path
-	cp config/init.vim          $nvimclipse_path
-	cp config/coc-settings.json $nvimclipse_path
-
-	sed -i "s|%clang_path%|$clang_path|g"           $nvimclipse_path/.cfg.chromatica
-	sed -i "s|%clang_version%|7.0.1|g"              $nvimclipse_path/.cfg.chromatica
-	sed -i "s|%nvimclipse_path%|$nvimclipse_path|g" $nvimclipse_path/.vimrc
-	sed -i "s|%nvimclipse_path%|$nvimclipse_path|g" $nvimclipse_path/.vimrc.plugins
-	sed -i "s|%nvimclipse_path%|$nvimclipse_path|g" $nvimclipse_path/init.vim
-	sed -i "s|%ccls_path%|$ccls_path|g"             $nvimclipse_path/coc-settings.json
-
-	mkdir ~/.config/nvim
-	ln -s $nvimclipse_path/coc-settings.json ~/.config/nvim/coc-settings.json
-
-	PATH=$PATH:$nodejs_path/bin $neovim_path/bin/nvim -u ./install.vim \
-	    +PlugInstall \
-	    +UpdateRemotePlugins \
-	    +":call coc#util#install()" \
-	    +qa
-}
-
-install_alias() {
-	echo "\nalias nv='PATH=$PATH:$nodejs_path/bin $neovim_path/bin/nvim -u $nvimclipse_path/init.vim'" >> ~/.bashrc
-}
-
-mkdir temp
-mkdir $thirdparty_path
-mkdir $nvimclipse_path
-
-install_clang
-install_ccls
-install_neovim
-install_nodejs
-install_nvimclipse
-install_alias
