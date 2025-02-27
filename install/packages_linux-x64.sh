06714ce9fec4449fec1c4e676892734227cbe567
xvoidee <alexander.chaika@gmail.com>
2025-02-27 16:09:06 +0100

    Update to new node & nvim runtimes

--- install/packages_linux-x64.sh
+++ install/packages_linux-x64.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-node_website="https://nodejs.org/dist/v18.18.0/"
-node_archive="node-v18.18.0-linux-x64.tar.xz"
-node_runtime="node-v18.18.0-linux-x64"
+node_website="https://nodejs.org/dist/v22.14.0/"
+node_archive="node-v22.14.0-linux-x64.tar.xz"
+node_runtime="node-v22.14.0-linux-x64"
 
-nvim_website="https://github.com/neovim/neovim/releases/download/v0.9.2/"
-nvim_archive="nvim-linux64.tar.gz"
-nvim_runtime="nvim-linux64"
-nvim_version="v0.9.2"
-
+nvim_website="https://github.com/neovim/neovim/releases/download/v0.10.4/"
+nvim_archive="nvim-linux-x86_64.tar.gz"
+nvim_runtime="nvim-linux-x86_64"
+nvim_version="v0.10.4"
