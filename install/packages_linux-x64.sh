085598e45828f01497ce08abd5f6da152a9c75db
xvoidee <alexander.chaika@gmail.com>
2025-04-15 21:52:01 +0200

    Update neovim to 0.11.0

--- install/packages_linux-x64.sh
+++ install/packages_linux-x64.sh
@@ -5,7 +5,7 @@ node_website="https://nodejs.org/dist/$node_version"
 node_archive="node-$node_version-linux-x64.tar.xz"
 node_runtime="node-$node_version-linux-x64"
 
-nvim_version="v0.10.4"
+nvim_version="v0.11.0"
 nvim_website="https://github.com/neovim/neovim/releases/download/$nvim_version"
 nvim_archive="nvim-linux-x86_64.tar.gz"
 nvim_runtime="nvim-linux-x86_64"
