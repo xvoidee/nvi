7ef05e7445d44ab758969dc86bf73d3187bce7c4
xvoidee <alexander.chaika@gmail.com>
2025-05-04 20:57:19 +0200

    Added plugin 'colorizer'

---
+++ config/.cfg.colorizer.lua
@@ -0,0 +1,13 @@
+require('colorizer').setup({
+  user_default_options = {
+    RGB      = true,
+    RRGGBB   = true,
+    names    = true,
+    RRGGBBAA = true,
+    rgb_fn   = true,
+    hsl_fn   = true,
+    css      = false,
+    css_fn   = false,
+    mode     = 'background',
+  }
+})
